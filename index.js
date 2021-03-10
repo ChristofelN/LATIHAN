e5e1065ab0fd12920fa60c45a49f8dbf3a04f28d
ChristofelN <73183415+ChristofelN@users.noreply.github.com>
2021-03-10 23:00:37 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,67 @@
-let hello;
-hello = "hello world  !!!";
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 33,
+};
 
-const birtYear = 200;
+//Hallo, nama saya John Doe. Umur saya 33 tahun.
+let kalimat =
+  "Hallo, nama saya " +
+  person.firstName +
+  " " +
+  person.lastName +
+  ". Umur saya " +
+  person.age +
+  " tahun";
 
-let age = 20;
-age += 2;
-let apakahSudahMenikah = false;
-let alamat;
+let kalimat6 = `Hallo, nama saya ${person.firstName} ${person.lastName}. Umur saya ${person.age} tahun`;
 
-console.log("hello");
-console.log(birtYear);
-console.log(age);
-console.log(apakahSudahMenikah);
-console.log(alamat);
+console.log(kalimat);
+console.log(kalimat6);
 
-let namaDepan ="Chris";
-let namaBelakang ="tofel";
+//Arrow Function
 
-let namaLengkap = namaDepan + "" + namaBelakang;
+function sayHello1() {}
 
-console.log(namaLengkap);
+const sayHello2 = function () {};
+
+const sayHello3 = (param1, param2 = "Adam") => `Hello ${param1} ${param2}`;
+
+console.log(sayHello3("Stenly", "Doe"));
+
+//Rest Parameter & Spread Operator
+const display = (param1, param2, ...rest) => {
+  console.log(param1, param2, rest);
+};
+
+display(1, 2, 3, 4, 5, 6);
+
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+const arr3 = [8, 9];
+
+// const combineArr = arr1.concat(arr2.concat(arr3));
+const combineArr = [...arr1, ...arr2, 7, ...arr3];
+const combineArr2 = [...combineArr];
+
+console.log(combineArr);
+
+const obj1 = { a: 1, b: 2 };
+const obj2 = { c: 3, d: 4 };
+
+const combineObj = { ...obj1, ...obj2 };
+console.log(combineObj);
+
+let person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 33,
+};
+
+person = {
+  ...person,
+  address: "Manado",
+  job: "Teacher",
+};
+
+console.log(person);
